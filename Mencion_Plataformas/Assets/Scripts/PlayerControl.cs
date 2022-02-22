395b17e87236998690c56a163077fcf54def5285
DESKTOP-C01HDPH\Xavi <xavicsilvente@gmail.com>
2022-02-22 19:56:44 +0100

    Ejercicio6

--- Mencion_Plataformas/Assets/Scripts/PlayerControl.cs
+++ Mencion_Plataformas/Assets/Scripts/PlayerControl.cs
@@ -6,16 +6,11 @@ public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speedPlayer = 8;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
     }
 
     private void FixedUpdate()
@@ -27,6 +22,9 @@ public class PlayerControl : MonoBehaviour
     private void Movement()
     {
         rb.velocity = new Vector2(speedPlayer * Input.GetAxis("Horizontal"), rb.velocity.y); // <--------------------- con este no vibra al colisionar con las paredes y objetos.
+
+        float movement = rb.velocity.magnitude;
+        anim.SetFloat("Speed", Mathf.Abs(movement));
     }
 
     private void DirDetect()
@@ -35,6 +33,7 @@ public class PlayerControl : MonoBehaviour
         {
             //cambiar de direccion el personaje derecha
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            
         }
         if (Input.GetAxis("Horizontal") < 0)
         {
