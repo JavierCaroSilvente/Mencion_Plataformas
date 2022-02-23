61a9256bcce415cc470af5202b3c12a0259be4c0
DESKTOP-C01HDPH\Xavi <xavicsilvente@gmail.com>
2022-02-23 23:05:18 +0100

    Ejercicio 7, 8 ,9

---
+++ Mencion_Plataformas/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float Speed = 8;
+    public GameObject Target;
+    private Vector3 FinalPos;
+
+    public float camOffSetX = 0;
+    public float camOffSetY = 0;
+    public float camOffSetZ = 0;
+
+    void Update()
+    {
+        if (Target.transform.parent != null)
+            FinalPos = new Vector3(Target.transform.position.x + camOffSetX, Target.transform.position.y + camOffSetY, camOffSetZ); 
+        transform.position = Vector3.Lerp(transform.position, FinalPos, Speed * Time.deltaTime);
+
+        if (transform.position.x < 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -1.9f)
+        {
+            transform.position = new Vector3(transform.position.x, 20, transform.position.z);
+        }
+        if (transform.position.y > 16f)
+        {
+            transform.position = new Vector3(transform.position.x, 16, transform.position.z);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Target.transform.parent == null)
+            FinalPos = new Vector3(Target.transform.position.x + camOffSetX, Target.transform.position.y + camOffSetY, camOffSetZ);
+        transform.position = Vector3.Lerp(transform.position, FinalPos, Speed * Time.deltaTime);
+
+        if (transform.position.x < 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -1.9f)
+        {
+            transform.position = new Vector3(transform.position.x, 20, transform.position.z);
+        }
+    }
+}
--- Mencion_Plataformas/Assets/Scripts/PlayerControl.cs
+++ Mencion_Plataformas/Assets/Scripts/PlayerControl.cs
@@ -7,37 +7,80 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D rb;
     public float speedPlayer = 8;
     public Animator anim;
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+    [SerializeField] bool isGrounded = false;
+
+    private bool jump = false;
+    private int currentJumps;
+    private int totalJumps;
+    public float jumpForce;
+    float movement;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        Jump();
+    }
 
     private void FixedUpdate()
     {
+       
+
         Movement();
         DirDetect();
+        GroundCheck();
     }
 
     private void Movement()
     {
         rb.velocity = new Vector2(speedPlayer * Input.GetAxis("Horizontal"), rb.velocity.y); // <--------------------- con este no vibra al colisionar con las paredes y objetos.
 
-        float movement = rb.velocity.magnitude;
-        anim.SetFloat("Speed", Mathf.Abs(movement));
+        if (isGrounded)
+        {
+            float movement = rb.velocity.x;
+            anim.SetFloat("MoveSpeed", Mathf.Abs(movement));
+        }
+    }
+
+    void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0 )
+        {
+            isGrounded = true;
+            anim.SetBool("IsGrounded", true);
+        }
+    }
+
+    private void Jump()
+    {
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        
+
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
+        {
+            anim.SetFloat("MoveSpeed", 0);
+            rb.velocity = new Vector2(rb.velocity.y, 0);
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+            anim.SetBool("IsGrounded", false);
+        }
     }
 
     private void DirDetect()
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
-            //cambiar de direccion el personaje derecha
             transform.rotation = Quaternion.Euler(0, 0, 0);
             
         }
         if (Input.GetAxis("Horizontal") < 0)
         {
-            //cambiar  de direccion personaje izquierda
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
